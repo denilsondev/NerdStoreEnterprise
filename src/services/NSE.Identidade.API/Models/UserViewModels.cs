95a4e883b35d8f70695b955ef939faddf12f5eda
denilsondev <denilsoncdc10@gmail.com>
2022-01-17 21:30:54 -0300

    controller autenticacao

---
+++ src/services/NSE.Identidade.API/Models/UserViewModels.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Identidade.API.Models
+{
+    public class UsuarioRegistro
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [EmailAddress(ErrorMessage = "O campo {0} esta em um formato invalido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "As senhas nao conferem")]
+        public string SenhaConfirmacao { get; set; }
+    }
+
+        public class UsuarioLogin
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [EmailAddress(ErrorMessage = "O campo {0} esta em um formato invalido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; }
+
+    }
+}
